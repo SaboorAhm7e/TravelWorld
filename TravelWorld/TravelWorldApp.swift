edef9a287249999a3d52cfa1ef0dfb9684c43996
saboor ahmad <saboorahm7e@gmail.com>
2025-05-07 07:46:34 -0700

    Initial Commit

---
+++ TravelWorld/TravelWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelWorldApp.swift
+//  TravelWorld
+//
+//  Created by saboor on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
