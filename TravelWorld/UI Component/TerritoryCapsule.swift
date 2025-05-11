2ab0d8946c4963a7d4b07b6a99666198f271aae9
saboor ahmad <saboorahm7e@gmail.com>
2025-05-11 00:10:43 -0700

    territory capsule and category

---
+++ TravelWorld/UI Component/TerritoryCapsule.swift
@@ -0,0 +1,42 @@
+//
+//  TerritoryCapsule.swift
+//  TravelWorld
+//
+//  Created by saboor on 10/05/2025.
+//
+
+import SwiftUI
+
+struct TerritoryCapsule: View {
+    // MARK: - Properties
+    @Environment(\.colorScheme) var colorScheme
+    var item : TerritoryModel
+    @Binding var selectedTerretoryType : TerritoryModel
+    
+    // MARK: - Body
+    var body: some View {
+        ZStack(alignment:.center) {
+            Capsule()
+                .fill(item == selectedTerretoryType ? Color.btnClr : (colorScheme == .dark ? Color.secondary.opacity(0.5) : Color.white))
+                .animation(.easeIn(duration:0.2))
+                .shadow(color: Color.black.opacity(0.3), radius: 8, x: 0, y: 5)
+            VStack {
+                ZStack {
+                    Circle()
+                        .fill(item == selectedTerretoryType ? (colorScheme == .dark ? Color.secondary : Color.white) : Color.btnClr)
+                        .frame(width:60,height: 60)
+                    Image(systemName: "\(item.icon)")
+                        .font(.title2)
+                        .foregroundColor(item == selectedTerretoryType ? (colorScheme == .dark ? Color.white : Color.btnClr) : (colorScheme == .dark ? Color.black : Color.white))
+                    
+                }
+                
+                Text(item.name)
+                    .foregroundColor(item == selectedTerretoryType ? Color.white : Color.black)
+                Spacer()
+            }
+            .padding(.vertical,10)
+        }
+    }
+}
+
--- TravelWorld/View/HomeView.swift
+++ TravelWorld/View/HomeView.swift
@@ -7,11 +7,68 @@
 
 import SwiftUI
 
+struct TerritoryModel: Hashable  {
+    let name : String
+    let icon : String
+}
+
 struct HomeView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
+    var territoryType : [TerritoryModel] = [
+        .init(name: "All", icon: "map"),
+        .init(name: "Hiking", icon: "location.circle"),
+        .init(name: "Forest", icon: "leaf"),
+        .init(name: "Camp", icon: "flame")
+    ]
+    var categories : [String] = [
+    "Popular",
+    "Adventure",
+    "Luxury",
+    "Couple",
+    "Family",
+    "Food",
+    "Friendly"
+    ]
+    @State var selectedTerretoryType : TerritoryModel
+    @State var selectedCategory : String
+    init() {
+        _selectedTerretoryType = State(initialValue: territoryType[0])
+        _selectedCategory = State(initialValue: categories[0])
+    }
     var body: some View {
         NavigationView {
             VStack {
+                HStack {
+                    ForEach(territoryType,id:\.self) { item in
+                        TerritoryCapsule(item: item, selectedTerretoryType: $selectedTerretoryType)
+                        .onTapGesture {
+                            selectedTerretoryType = item
+                        }
+                    }
+                }
+                .frame(height:140)
+                .padding(.horizontal,16)
                 
+                Spacer()
+                    .frame(height:30)
+                
+                ScrollView(.horizontal,showsIndicators: false) {
+                    HStack(spacing:15) {
+                        ForEach(categories,id: \.self) { item in
+                            Text(item)
+                                .font(.title3)
+                                .foregroundColor(selectedCategory == item ? (colorScheme == .dark ? Color.white : Color.black) : Color.secondary)
+                                .onTapGesture {
+                                    selectedCategory = item
+                                }
+                        }
+                    }
+                }
+                .padding(.horizontal,16)
+                
+                
+               Spacer()
             }
             .navigationTitle("Places")
             .toolbar {
@@ -20,6 +77,7 @@ struct HomeView: View {
                         print("hello")
                     } label: {
                         Image(systemName: "gear")
+                            .foregroundColor(Color.btnClr)
                     }
 
                 }
@@ -30,6 +88,11 @@ struct HomeView: View {
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        Group {
+            HomeView()
+                .preferredColorScheme(.light)
+            HomeView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
